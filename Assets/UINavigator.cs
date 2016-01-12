fb3d8964143993ad24a789257489a631e7f328ec
bcjroberts <bcjroberts@gmail.com>
2016-01-12 12:04:58 -0700

    Started working on UI. Added UI script, leaving multiplayer till later

---
+++ Assets/UINavigator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UINavigator : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject singleplayerMenu;
+    public GameObject multiplayerMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void goToMenu(int menuNum)
+    {
+        mainMenu.SetActive(false);
+        singleplayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        switch (menuNum)
+        {
+            case 0:
+                mainMenu.SetActive(true);
+                break;
+            case 1:
+                singleplayerMenu.SetActive(true);
+                break;
+            case 2:
+                multiplayerMenu.SetActive(true);
+                break;
+        }
+    }
+	
+	
+}
